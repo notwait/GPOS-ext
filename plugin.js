f25b9a04ef30bb82ceead5d0aea54a470cd2e47f
notwait <71197732+notwait@users.noreply.github.com>
2021-10-24 14:08:41 -0300

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,3 +1,3 @@
-function pad(p){
-  return navigator.getGamepads()[p]
-}
+var controller = navigator.getGamepads()[0]
+var buttons = controller.buttons
+setTimeout(() => {setCloud('controller', `${Number(buttons[0].pressed)}${Number(buttons[1].pressed)}${Number(buttons[2].pressed)}${Number(buttons[3].pressed)}`)}, 0)
