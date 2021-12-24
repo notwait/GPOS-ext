11f7ab5157e0fdb652787fca5311e4842e64ee03
notwait <71197732+notwait@users.noreply.github.com>
2021-12-24 15:25:10 -0300

    Delete plugin.js

--- plugin.js
+++
@@ -1,5 +0,0 @@
-var buttons = [false, false, false, false]
-window.ongamepadconnected = (e) => {
-  buttons = e.gamepad.buttons
-}
-setTimeout(() => {setCloud('controller', `${Number(buttons[0].pressed)}${Number(buttons[1].pressed)}${Number(buttons[2].pressed)}${Number(buttons[3].pressed)}`)}, 0)
